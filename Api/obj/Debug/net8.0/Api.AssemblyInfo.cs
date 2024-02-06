88aa43813db1f1707217c4e3ee820bdba39e641c
Benjamin Carter <benjamincarter@caresolutions.com>
2024-02-06 13:32:00 -0500

    Configured Entity Frameworks to work with Application

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e7993c8c7f8039b8ba6557e118491374940e60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677d2980d6cd72b28cc6d9e5873897a1b2a6fd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
