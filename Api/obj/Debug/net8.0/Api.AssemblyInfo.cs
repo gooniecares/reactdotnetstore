69ab9d40a82e439571e08e16e2a125b982890d28
Benjamin Carter <benjamincarter@caresolutions.com>
2024-02-06 11:28:53 -0500

    ReCreated Web Api With Controllers

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2f378ad5ee23a16fa36c44ef88d7304573096c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886acc20005faa336857ccdf9644e108b1956f13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
