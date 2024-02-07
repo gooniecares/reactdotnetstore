73e7ce8a9e1c3a0a01159bb854e633476b81e29b
Benjamin Carter <benjamincarter@caresolutions.com>
2024-02-06 19:27:48 -0500

    Converted context to private _context

--- Api/Controllers/ProductsController.cs
+++ Api/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Api.Data;
 using Api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -12,26 +13,26 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
+
         public ProductsController(StoreContext context)
         {
-            this.context = context;
+            _context = context;
+
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-
-            return  Ok(products);
+            return await _context.Products.ToListAsync();
 
         }
 
         [HttpGet("{id}")]   //  api/products/3
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
 
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
 
         }
     }
